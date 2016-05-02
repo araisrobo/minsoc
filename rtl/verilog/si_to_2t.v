0bcd0bb1772efc979214304d05d318f0aff30b20
Yishin Li <ysli@araisrobo.com>
2016-05-02 18:37:49 +0800

    si_to_2t.v: simplify to save area

--- rtl/verilog/si_to_2t.v
+++ rtl/verilog/si_to_2t.v
@@ -89,9 +89,9 @@ input			t1_wb_ack_i;
 //
 // Internal wires & registers
 //
-wire	[`TC_IIN_W-1:0]	i0_in;
+wire	[`TC_AW+`TC_BSW+1+`TC_DW-1:0]   i0_in_common;
+wire	[1:0]	                        i0_in_ctrl;
 wire	[`TC_TIN_W-1:0]	i0_out;
-wire	[`TC_IIN_W-1:0]	t0_out, t1_out;
 wire	[`TC_TIN_W-1:0]	t0_in, t1_in;
 wire	[1:0]		req_t;
 wire                    accel_sel;
@@ -99,22 +99,20 @@ wire                    accel_sel;
 //
 // Group WB initiator 0 i/f inputs and outputs
 //
-assign i0_in = {i0_wb_cyc_i, i0_wb_stb_i, i0_wb_adr_i,
-		i0_wb_sel_i, i0_wb_we_i, i0_wb_dat_i};
+assign i0_in_common = {i0_wb_adr_i, i0_wb_sel_i, i0_wb_we_i, i0_wb_dat_i};
+assign i0_in_ctrl = {i0_wb_cyc_i, i0_wb_stb_i};
 assign {i0_wb_dat_o, i0_wb_ack_o} = i0_out;
 
 //
 // Group WB target 0 i/f inputs and outputs
 //
-assign {t0_wb_cyc_o, t0_wb_stb_o, t0_wb_adr_o,
-		t0_wb_sel_o, t0_wb_we_o, t0_wb_dat_o} = t0_out;
+assign {t0_wb_adr_o, t0_wb_sel_o, t0_wb_we_o, t0_wb_dat_o} = i0_in_common;
 assign t0_in = {t0_wb_dat_i, t0_wb_ack_i};
 
 //
 // Group WB target 1 i/f inputs and outputs
 //
-assign {t1_wb_cyc_o, t1_wb_stb_o, t1_wb_adr_o,
-		t1_wb_sel_o, t1_wb_we_o, t1_wb_dat_o} = t1_out;
+assign {t1_wb_adr_o, t1_wb_sel_o, t1_wb_we_o, t1_wb_dat_o} = i0_in_common;
 assign t1_in = {t1_wb_dat_i, t1_wb_ack_i};
 
 //
@@ -122,8 +120,8 @@ assign t1_in = {t1_wb_dat_i, t1_wb_ack_i};
 //
 // Either inputs from the initiator are assigned or zeros.
 //
-assign t0_out = req_t[0] ? i0_in : {`TC_IIN_W{1'b0}};
-assign t1_out = req_t[1] ? i0_in : {`TC_IIN_W{1'b0}};
+assign {t0_wb_cyc_o, t0_wb_stb_o} = req_t[0] ? i0_in_ctrl : {2'b00};
+assign {t1_wb_cyc_o, t1_wb_stb_o} = req_t[1] ? i0_in_ctrl : {2'b00};
 
 //
 // Assign to WB initiator i/f outputs
--- rtl/verilog/tc_defines.v
+++ rtl/verilog/tc_defines.v
@@ -16,16 +16,15 @@
 //
 // Width of WB target inputs (coming from WB slave)
 //
-// data bus width + ack + err
+// data bus width + ack
 //
-`define TC_TIN_W	`TC_DW+1+1
+`define TC_TIN_W	`TC_DW+1
 
 //
 // Width of WB initiator inputs (coming from WB masters)
 //
 // cyc + stb + address bus width +
 // byte select bus width + we + data bus width
-// (remove wb_err)
 `define TC_IIN_W	1+1+`TC_AW+`TC_BSW+1+`TC_DW
 
 
